70a00ed6aceaf806d6c5c06853c90838486a0d3f
Laura Cárdenas <lacardenasv@unal.edu.co>
2018-05-03 09:14:09 -0500

    test

--- app/sockets/index.js
+++ app/sockets/index.js
@@ -1,7 +1,9 @@
 import redisClient from '../services/redis'
 
 function initializeSocket(server){
-    const io = require('socket.io')(server);
+    const io = require('socket.io')(server,{
+      path: '/board/socket.io'
+    });
     io.on('connection', (socket) => {
         var room = socket.handshake['query']['room'];
         var nick = socket.handshake['query']['nick'];
